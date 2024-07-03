ac7539416753284f3e7d085b76f669a26b06331c
HeyZeusV <valdesj419@gmail.com>
2024-07-03 00:50:45 -0700

    Created Composable that displays list name and number of items checked off. It currently has all its data hard coded. Added functions to help shorten resource calls in Composables.

--- app/src/main/java/com/heyzeusv/yourlists/lists/ListMenu.kt
+++ app/src/main/java/com/heyzeusv/yourlists/lists/ListMenu.kt
@@ -1,6 +1,27 @@
 package com.heyzeusv.yourlists.lists
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.tooling.preview.Preview
+import com.heyzeusv.yourlists.R
+import com.heyzeusv.yourlists.util.PreviewUtil
+import com.heyzeusv.yourlists.util.dRes
+import com.heyzeusv.yourlists.util.pRes
+import com.heyzeusv.yourlists.util.sRes
 
 @Composable
 fun ListMenu() {
@@ -9,7 +30,51 @@ fun ListMenu() {
 
 @Composable
 fun ListMenuItem() {
-
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(dRes(R.dimen.card_radius)),
+    ) {
+        Column(modifier = Modifier.padding(all = dRes(R.dimen.lmi_padding_all))) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "List Title",
+                    style = MaterialTheme.typography.headlineMedium
+                )
+                Icon(
+                    painter = pRes(R.drawable.button_options),
+                    contentDescription = sRes(R.string.button_options_cdesc),
+                )
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(dRes(R.dimen.lmi_progress_spacedBy)),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                LinearProgressIndicator(
+                    progress = { .75f },
+                    modifier = Modifier
+                        .height(dRes(R.dimen.lmi_progress_height))
+                        .weight(1f),
+                    trackColor = MaterialTheme.colorScheme.background,
+                    strokeCap = StrokeCap.Round
+                )
+                Text(text = "8/10")
+            }
+        }
+    }
 }
 
+@Preview
+@Composable
+private fun ListMenuItemPreview() {
+    PreviewUtil.apply {
+        Preview {
+            ListMenuItem()
+        }
+    }
+}
 
--- app/src/main/java/com/heyzeusv/yourlists/util/ExtensionFunctions.kt
+++ app/src/main/java/com/heyzeusv/yourlists/util/ExtensionFunctions.kt
@@ -1,9 +1,15 @@
 package com.heyzeusv.yourlists.util
 
+import androidx.annotation.DimenRes
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
 
 /**
  *  Load a string resource with formatting.
@@ -15,3 +21,22 @@ import androidx.compose.ui.res.stringResource
 @Composable
 @ReadOnlyComposable
 fun sRes(@StringRes id: Int, vararg args: Any): String = stringResource(id, *args)
+
+/**
+ *  Create a [Painter] from an Android resource id.
+ *
+ *  @param id Resources object to query the image file from.
+ *  @return [Painter] used for drawing the loaded resource.
+ */
+@Composable
+fun pRes(@DrawableRes id: Int): Painter = painterResource(id)
+
+/**
+ *  Load a dimension resource.
+ *
+ *  @param id The resource identifier.
+ *  @return The dimension value associated with the resource.
+ */
+@Composable
+@ReadOnlyComposable
+fun dRes(@DimenRes id: Int): Dp = dimensionResource(id)
