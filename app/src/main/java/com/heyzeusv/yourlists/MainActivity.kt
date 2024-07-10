01235dfb0a190e30460c7d81ea9d6581536a16e1
HeyZeusV <valdesj419@gmail.com>
2024-07-10 05:29:11 -0700

    Handled overflow for TopAppBar.

--- app/src/main/java/com/heyzeusv/yourlists/MainActivity.kt
+++ app/src/main/java/com/heyzeusv/yourlists/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -114,6 +115,8 @@ fun YourListsTopAppBar(si: ScaffoldInfo) {
         title = {
             Text(
                 text = title,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1,
                 style = MaterialTheme.typography.headlineSmall
             )
         },
