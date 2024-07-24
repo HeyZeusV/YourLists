1470c6c7cbd61e465a1a6c5f172b1bca4b9ab08c
HeyZeusV <valdesj419@gmail.com>
2024-07-24 15:44:27 -0700

    Updated version code/name for v1.2.0 release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.heyzeusv.yourlists"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.1.0"
+        versionCode = 4
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
